b18a95118a91a306bf5a8799e1fe53e5172d0b00
Muralivarma13 <muralitirumani117@gmail.com>
2025-12-14 18:09:37 +0530

    fix django module paths for render

--- deployproject/deployproject/settings.py
+++ deployproject/deployproject/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'deployproject.urls'
+ROOT_URLCONF = 'deployproject.deployproject.urls'
 
 TEMPLATES = [
     {
